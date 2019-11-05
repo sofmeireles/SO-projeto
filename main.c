d7084521aa0dff0ef9126181e9a53e8c930b96e5
sofmeireles <sofiamfc24964@gmail.com>
2019-11-05 21:10:52 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,16 @@
-#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+#include <sys/stat.h>   
+#include <fcntl.h> 
+#include <sys/types.h> 
+#include <sys/wait.h> 
+#include <unistd.h>
+#include <errno.h>
 #include <string.h>
 
 struct config{
@@ -21,7 +32,7 @@ void print_struct(struct config* conf){
     printf("hmin: %d, holmax: %d\n",conf->hold_min,conf->hold_max);
     printf("qtdp: %d\n",conf->qtd_max_partidas);
     printf("qtdc: %d\n",conf->qtd_max_chegadas);
-};
+}
 
 
 struct config* read_config(struct config* conf){
@@ -30,10 +41,23 @@ struct config* read_config(struct config* conf){
     return conf;
 }
 
-
-int main() {
+void inicia(){
     struct config* config=malloc(sizeof(struct config));
     config=read_config(config);
     //print_struct(config);
+    pid_t processo;
+    processo=fork();
+    if(processo==0){
+        printf("ID da torre de controlo: %d\n",pid());
+    }
+
+    else{
+        printf("Nao sou a torre de controlo!\n");
+    }
+}
+
+
+int main() {
+    inicia();
     return 0;
 }
